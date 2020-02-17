2521b6a7438f652ab501ec1c4425241dcc145de9
Denys Shabalin <shabalin@google.com>
2020-02-17 12:20:44 +0100

    Drop inline annotations for now

--- Sources/GenericExamples/AdditiveArithmeticGeneric.swift
+++ Sources/GenericExamples/AdditiveArithmeticGeneric.swift
@@ -12,12 +12,10 @@ public protocol AdditiveArithmeticGeneric {
 
 extension Case: AdditiveArithmeticGeneric
 where A: AdditiveArithmeticGeneric, B: AdditiveArithmeticGeneric {
-    @inline(__always)
     public static var zero: Self {
         fatalError("'zero' cannot be synthesized for sum types")
     }
 
-    @inline(__always)
     public static func + (lhs: Self, rhs: Self) -> Self {
         switch (lhs, rhs) {
         case let (.of(index, x), .of(_, y)):
@@ -32,36 +30,30 @@ where A: AdditiveArithmeticGeneric, B: AdditiveArithmeticGeneric {
 
 extension Field: AdditiveArithmeticGeneric
 where A: AdditiveArithmeticGeneric, B: AdditiveArithmeticGeneric {
-    @inline(__always)
     public static var zero: Self {
         return Field(A.zero, B.zero)
     }
 
-    @inline(__always)
     public static func + (lhs: Self, rhs: Self) -> Self {
         return Field(lhs.value + rhs.value, lhs.next + rhs.next)
     }
 }
 
 extension Struct: AdditiveArithmeticGeneric where A: AdditiveArithmeticGeneric {
-    @inline(__always)
     public static var zero: Self {
         return Struct(A.zero)
     }
 
-    @inline(__always)
     public static func + (lhs: Self, rhs: Self) -> Self {
         return Struct(lhs.shape + rhs.shape)
     }
 }
 
 extension Enum: AdditiveArithmeticGeneric where A: AdditiveArithmeticGeneric {
-    @inline(__always)
     public static var zero: Self {
         return Enum(A.zero)
     }
 
-    @inline(__always)
     public static func + (lhs: Self, rhs: Self) -> Self {
         return Enum(lhs.shape + rhs.shape)
     }
@@ -70,12 +62,10 @@ extension Enum: AdditiveArithmeticGeneric where A: AdditiveArithmeticGeneric {
 // Base cases.
 
 extension Empty: AdditiveArithmeticGeneric {
-    @inline(__always)
     public static var zero: Self {
         return Empty()
     }
 
-    @inline(__always)
     public static func + (lhs: Self, rhs: Self) -> Self {
         return Empty()
     }
