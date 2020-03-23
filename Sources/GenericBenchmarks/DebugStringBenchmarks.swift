78a4aa8e6ba51a1ef3e6eb9f6712b8f4b5e1b78c
Denys Shabalin <shabalin@google.com>
2020-03-23 14:57:01 +0100

    Generalize debug string benchmarks to PointN

--- Sources/GenericBenchmarks/DebugStringBenchmarks.swift
+++ Sources/GenericBenchmarks/DebugStringBenchmarks.swift
@@ -1,8 +1,32 @@
+// ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 1)
 import Benchmark
 import GenericCore
 import GenericExamples
 
 let debugStringBenchmarks = BenchmarkSuite(name: "DebugString") { suite in
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point1 (generic)") {
+        stringSink = p1_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point1 (reference)") {
+        stringSink = String(reflecting: p1_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point2 (generic)") {
+        stringSink = p2_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point2 (reference)") {
+        stringSink = String(reflecting: p2_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
     suite.benchmark("Point3 (generic)") {
         stringSink = p3_1.debugDescriptionGeneric
     }
@@ -10,6 +34,138 @@ let debugStringBenchmarks = BenchmarkSuite(name: "DebugString") { suite in
     suite.benchmark("Point3 (reference)") {
         stringSink = String(reflecting: p3_1)
     }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point4 (generic)") {
+        stringSink = p4_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point4 (reference)") {
+        stringSink = String(reflecting: p4_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point5 (generic)") {
+        stringSink = p5_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point5 (reference)") {
+        stringSink = String(reflecting: p5_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point6 (generic)") {
+        stringSink = p6_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point6 (reference)") {
+        stringSink = String(reflecting: p6_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point7 (generic)") {
+        stringSink = p7_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point7 (reference)") {
+        stringSink = String(reflecting: p7_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point8 (generic)") {
+        stringSink = p8_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point8 (reference)") {
+        stringSink = String(reflecting: p8_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point9 (generic)") {
+        stringSink = p9_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point9 (reference)") {
+        stringSink = String(reflecting: p9_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point10 (generic)") {
+        stringSink = p10_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point10 (reference)") {
+        stringSink = String(reflecting: p10_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point11 (generic)") {
+        stringSink = p11_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point11 (reference)") {
+        stringSink = String(reflecting: p11_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point12 (generic)") {
+        stringSink = p12_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point12 (reference)") {
+        stringSink = String(reflecting: p12_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point13 (generic)") {
+        stringSink = p13_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point13 (reference)") {
+        stringSink = String(reflecting: p13_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point14 (generic)") {
+        stringSink = p14_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point14 (reference)") {
+        stringSink = String(reflecting: p14_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point15 (generic)") {
+        stringSink = p15_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point15 (reference)") {
+        stringSink = String(reflecting: p15_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 8)
+
+    suite.benchmark("Point16 (generic)") {
+        stringSink = p16_1.debugDescriptionGeneric
+    }
+
+    suite.benchmark("Point16 (reference)") {
+        stringSink = String(reflecting: p16_1)
+    }
+
+    // ###sourceLocation(file: "/usr/local/google/home/shabalin/swift-libs/swift-shaped/Sources/GenericBenchmarks/DebugStringBenchmarks.swift.gyb", line: 18)
 
     suite.benchmark("BinaryTree (generic)") {
         stringSink = tree1.debugDescriptionGeneric
