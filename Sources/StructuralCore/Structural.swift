e52bb75a442122d2d1e7bcd2826d908ee6032c2e
Denys Shabalin <shabalin@google.com>
2020-08-05 17:34:22 +0200

    Drop unnecessary Foundation import

--- Sources/StructuralCore/Structural.swift
+++ Sources/StructuralCore/Structural.swift
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
-
 /// A type that can be converted to and from its structural representation.
 public protocol Structural {
     /// A structural representation for `Self`.
